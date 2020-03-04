03bbb337c03f20712681b8024de5ee8b82c666b0
Mustafa Aslanov <44026469+aslanovmustafa@users.noreply.github.com>
2020-03-04 20:30:41 +0400

    Update RandomSleep.java

--- Readers Writers problem using Semaphore/RandomSleep.java
+++ Readers Writers problem using Semaphore/RandomSleep.java
@@ -1,12 +1,13 @@
+//increased sleep time
+
 import java.util.Random;
 public class RandomSleep {
     public void doSleep() {
     	  Random rnd = new Random();
-    	  int rndInt = rnd.nextInt(1000-100)+100; //randomly assigns time to sleep
+    	  int rndInt = rnd.nextInt(3000-1000)+1000; //randomly assigns time to sleep
     	  try {
 			Thread.sleep(rndInt);
 		} catch (InterruptedException e) {}
       
   }
 }
-
